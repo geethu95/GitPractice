d38c3dcde1a0f5ea3d5dc85ae88b14ee3005d015
geethu95 <geethua95@gmail.com>
2022-10-12 14:07:16 +0530

    Hellowworld

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+class Assignment{
+	public static void main(String args[]){
+		int a=12,b=6,c;
+		float f1=2.5f,f2,h=12.5f,area;
+		c=a+b;
+		f2=Float.sum(a,f1);
+		area=(b*h)/2;
+		System.out.println("Sum of integer numbers is "+ c);
+		System.out.println("Sum of integer and float is " + f2);
+		System.out.println("Area of triangle is "+ area);
+		System.out.println("Name is Geethu");
+		System.out.println("Age is 27");
+	}
+}
